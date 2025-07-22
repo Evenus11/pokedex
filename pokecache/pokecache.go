ee7abfeba81b5ec237e1576f112821d3e68e7e33
Evenus11 <ewen.shackel@gmail.com>
2025-07-22 12:03:28 +0100

    cache get and add functions built

--- pokecache/pokecache.go
+++ pokecache/pokecache.go
@@ -26,4 +26,24 @@ func NewCache(i time.Duration) *Cache {
 
 }
 
-func (c *Cach) Add()
+func (c *Cache) Add(key string, val []byte) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.entries[key] = cacheEntry{
+		createdAt: time.Now(),
+		val:       val,
+	}
+
+}
+
+func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	res, ok := c.entries[key]
+
+	if !ok {
+		return nil, false
+	}
+	return res.val, true
+
+}
